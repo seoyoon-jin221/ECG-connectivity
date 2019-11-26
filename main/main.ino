0bed31e8d5aa0f446b9cf4728e0d8c2421f91b5e
Seoyoon <seoyoon.jin221@gmail.com>
2019-11-26 14:29:34 -0800

    updated main.ino

--- main/main.ino
+++ main/main.ino
@@ -8,51 +8,31 @@
  * RX is digital pin 2 (connect to TX of other device)
  * TX is digital pin 3 (connect to RX of other device)
 
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
- This example code is in the public domain.
-
  */
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(2, 3); // RX, TX
 
-void setup()
-{
-  // Open serial communications and wait for port to open:
+
+int pin = A0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin, INPUT);
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Native USB only
   }
-  Serial.println("computer Serial connected");
-  // set the data rate for the SoftwareSerial port
+  Serial.println("\n");
   mySerial.begin(9600);
   mySerial.println("TX/RX serial connected");
 }
 
-rad = 0
-void loop() // run over and over
-{
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(sin(rad));
-  rad++;
-  // Example code for read and write
+void loop() {
+  // put your main code here, to run repeatedly:
+  double vOut = analogRead(pin);
+  //double vOut1 = map(vOut, 0, 1023, 0, 251);
+  Serial.println(vOut);
   /*
   if (mySerial.available())
     Serial.write(mySerial.read());
